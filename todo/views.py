35804ae6a8e4e966712fbdae55e39d8bdbefa660
IsaacShrestha@github.com <ishrestha@unomaha.edu>
2016-12-07 16:54:22 -0800

    view registered in urls.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from models import TodoItem
+from serializers import TodoItemSerializer
 
-# Create your views here.
+
+class TodoItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows TodoItems to be CRUDed.
+    """
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
