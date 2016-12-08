90f344fece08e2c73ee1a786018ace443b6a7c68
IsaacShrestha@github.com <ishrestha@unomaha.edu>
2016-12-07 20:30:21 -0800

    prepared for authentication

--- todo_django/urls.py
+++ todo_django/urls.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 
 from rest_framework import routers
 from todo.views import TodoItemViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register("todos", TodoItemViewSet)
 
 urlpatterns = [
+	url(r'^api-auth-token/', obtain_auth_token),
     url(r'^api/', include(router.urls)),
 ]
 
